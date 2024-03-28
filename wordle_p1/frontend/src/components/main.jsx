be214dea2e43a7415333a50f113c9a7a929becfb
Jenny Ho <jenny.ho@mail.utoronto.ca>
2024-03-27 21:52:48 -0400

    step 01 done

--- wordle_p1/frontend/src/components/main.jsx
+++ wordle_p1/frontend/src/components/main.jsx
@@ -9,6 +9,16 @@ class Header extends React.Component {
     return (
 		<header>
 			<nav>
+				<span className="alignleft"></span>
+				<span className="aligncenter">
+					<a className="ui_home" style={{'font-size':'x-large', 'text-decoration':'underline'}}>309DLE</a>
+				</span>
+				<span class="alignright">
+					<a className="ui_username"><span className="material-symbols-outlined"> person </span></a>
+					<a className="ui_play"><span className="material-symbols-outlined"> play_circle </span></a>
+					<a className="ui_stats"><span className="material-symbols-outlined"> leaderboard </span></a>
+					<a className="ui_instructions"><span className="material-symbols-outlined"> help </span></a>
+				</span>
 			</nav>
 		</header>
     );
