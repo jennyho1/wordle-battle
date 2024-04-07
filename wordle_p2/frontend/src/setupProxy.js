9cb9e3162eab3328d48c3f706580c4253704f911
Jenny Ho <jenny.ho@mail.utoronto.ca>
2024-04-07 00:10:31 -0400

    debug4

--- wordle_p2/frontend/src/setupProxy.js
+++ wordle_p2/frontend/src/setupProxy.js
@@ -7,4 +7,13 @@ module.exports = function (app) {
             changeOrigin: true,
         })
     );
+		app.use('/ws', createProxyMiddleware({ target: 'ws://localhost:8552', ws: true }));
 };
+
+
+
+// // Forwards HTTP requests to your frontend running on port 8551
+// app.use('/', createProxyMiddleware({ target: 'http://localhost:8551', ws: true }));
+
+// // Forwards WebSocket requests to your WebSocket server running on port 8552
+// app.use('/ws', createProxyMiddleware({ target: 'ws://localhost:8552', ws: true }));
